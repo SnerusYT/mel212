c839e319fe064193d026904a6f875a73bfa71cff
Jeremy Martins <jeremy200316@gmail.com>
2025-09-19 04:06:26 +0200

    Added new Vid

--- src/data/videos.js
+++ src/data/videos.js
@@ -2,7 +2,8 @@
 // Import your video thumbnails and files
 import papillon1 from '../assets/videos/merter_papillon.mov';
 import papillonThumbnail from '../assets/images/mertert_papillon_4.jpg';
-
+import princess1 from '../assets/videos/mertert_princess_twirl.mov';
+import princess2 from '../assets/images/thumbnail_princess.jpg';
 export const videos = [
   {
     id: 1,
@@ -10,6 +11,13 @@ export const videos = [
     description: "Um jardim cheio de borboletas",
     thumbnail: papillonThumbnail,
     videoSrc: papillon1,
+  },
+  {
+    id: 2,
+    title: "A minha Princesa",
+    description: "Ufffff que princesa é esta ?",
+    thumbnail: princess2,
+    videoSrc: princess1,
   },
   // Add more videos...
 ];
